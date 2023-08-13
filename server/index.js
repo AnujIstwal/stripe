b3765c042f646ddf66fbefffee22903e407c3639
AnujIstwal <99342265+AnujIstwal@users.noreply.github.com>
2023-08-13 10:18:49 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,6 +15,11 @@ const port = 8000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use("/", require("./routes/authRoutes"));
 
